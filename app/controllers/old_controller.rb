108570c9b14f54ba283eb6459093ad5882e17f71
yuknak <yuknak4@gmail.com>
2019-01-09 13:16:41 +0900

    Add initial old page

---
+++ app/controllers/old_controller.rb
@@ -0,0 +1,14 @@
+class OldController < ApplicationController
+  def index
+    
+    site_id = params[:s]
+    redirect_to :root if site_id.blank?
+    
+    @site = Site.find(site_id)
+    redirect_to :root if @site.blank?
+    
+    @articles = Article.where(site_id: @site.id, post_time: 2.weeks.ago..Time.now).order('post_time desc')
+    redirect_to :root if @articles.nil?
+  
+  end
+end
