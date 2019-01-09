0a7b5e342c9216b53f831e23a6da49379dda94b1
yuknak <yuknak4@gmail.com>
2019-01-09 10:36:17 +0900

    Fix article_out_counts process

--- app/controllers/out_controller.rb
+++ app/controllers/out_controller.rb
@@ -8,20 +8,24 @@ class OutController < ApplicationController
     last_time = Time.new
     count_date = last_time.localtime.strftime("%Y%m%d")
     
-    daily_out_count = DailyOutCount.find_or_initialize_by(
-      site_id: site_id.to_i,
-      count_date: count_date.to_i,
-    )
-    daily_out_count.count += 1
-    daily_out_count.save! 
+    if (site_id.present?) then
+        daily_out_count = DailyOutCount.find_or_initialize_by(
+        site_id: site_id.to_i,
+        count_date: count_date.to_i,
+      )
+      daily_out_count.count += 1
+      daily_out_count.save! 
+    end
     
-    article_out_counts = ArticleOutCount.find_or_initialize_by(
-      article_id: article_id.to_i,
-    )
-    article_out_counts.last_time = last_time
-    article_out_counts.count += 1
-    article_out_counts.save!
+    if (article_id.present?) then
+      article_out_counts = ArticleOutCount.find_or_initialize_by(
+        article_id: article_id.to_i,
+      )
+      article_out_counts.last_time = last_time
+      article_out_counts.count += 1
+      article_out_counts.save!
+    end
 
-    redirect_to url
+    redirect_to url if url.present?
   end
 end
