1ac9b7edd3d9371058cc3ce02be058771d0b3bff
yuknak <yuknak4@gmail.com>
2019-01-09 10:11:32 +0900

    Add article_out_count process

--- app/controllers/out_controller.rb
+++ app/controllers/out_controller.rb
@@ -1,14 +1,27 @@
 class OutController < ApplicationController
   def index
-    site_id = params[:s];
+    
+    site_id = params[:s]
+    article_id = params[:a]
     url = params[:u]
-    count_date = Time.new.localtime.strftime("%Y%m%d")
+    
+    last_time = Time.new
+    count_date = last_time.localtime.strftime("%Y%m%d")
+    
     daily_out_count = DailyOutCount.find_or_initialize_by(
       site_id: site_id.to_i,
       count_date: count_date.to_i,
     )
-    daily_out_count.count += 1;
+    daily_out_count.count += 1
     daily_out_count.save! 
+    
+    article_out_counts = ArticleOutCount.find_or_initialize_by(
+      article_id: article_id.to_i,
+    )
+    article_out_counts.last_time = last_time
+    article_out_counts.count += 1
+    article_out_counts.save!
+
     redirect_to url
   end
 end
