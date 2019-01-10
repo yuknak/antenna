d222dd2faec05dc06264c2cb267e63d2ba0e981d
yuknak <yuknak4@gmail.com>
2019-01-10 10:38:33 +0900

    Fix schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,33 +1,11 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-# Rails.rootを使用するために必要
+# To run:
+# bundle exec whenever -i
 
 require File.expand_path(File.dirname(__FILE__) + "/environment")
-# cronを実行する環境変数
 rails_env = ENV['RAILS_ENV'] || :development
-# cronを実行する環境変数をセット
 set :environment, rails_env
-# cronのログの吐き出し場所
 set :output, "#{Rails.root}/log/cron.log"
-
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
 
 every 10.minutes do
   rake "rss:fetch"
 end
-
-# Learn more: http://github.com/javan/whenever
