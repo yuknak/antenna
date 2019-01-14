26df8e8069a6352ad07c160a84700214e280a341
yuknak <yuknak4@gmail.com>
2019-01-14 20:37:58 +0900

    Add Rails.cache to top page

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,19 +1,46 @@
 class TopController < ApplicationController
+  
+  private def cached_all_articles
+    Rails.cache.fetch("/model/article/all", expired_in: 10.minutes) do
+      Article.order('post_time desc').limit(150)
+    end
+  end
+  private def cached_category_id_articles
+    Rails.cache.fetch("/model/article/category/#{@category_id}", expired_in: 10.minutes) do
+      Article.where(category_id: @category_id).order('post_time desc').limit(150)
+    end
+  end
+  private def cached_site_id_articles(site_id)
+    Rails.cache.fetch("/model/article/site/#{site_id}", expired_in: 10.minutes) do
+      Article.where(site_id: site_id).order('post_time desc').limit(5)
+    end
+  end
+  private def cached_all_sites
+    Rails.cache.fetch("/model/site/all", expired_in: 10.minutes) do
+      Site.order('week_out_count desc').limit(20)
+    end
+  end
+  private def cached_catgory_id_sites
+    Rails.cache.fetch("/model/site/category/#{@category_id}", expired_in: 10.minutes) do
+      Site.where(category_id: @category_id).order('week_out_count desc').limit(20)
+    end
+  end
+
   def index
     
     @category_id = params[:id]
 
     if @category_id.blank? then
-      @articles = Article.order('post_time desc').limit(150)
-      @sites = Site.order('week_out_count desc').limit(20)
+      @articles = cached_all_articles
+      @sites = cached_all_sites
     else
-      @articles = Article.where(category_id: @category_id).order('post_time desc').limit(150)
-      @sites = Site.where(category_id: @category_id).order('week_out_count desc').limit(20)
+      @articles = cached_category_id_articles
+      @sites = cached_catgory_id_sites
     end
 
     @site_articles = []
     @sites.each { |site|
-      articles = Article.where(site_id: site.id).order('post_time desc').limit(5)
+      articles = cached_site_id_articles(site.id)
       @site_articles.push(articles)
     }
 
