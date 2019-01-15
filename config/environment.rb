48951143f868dc6e0421582897996e2d62a561ac
yuknak <yuknak4@gmail.com>
2019-01-15 10:23:29 +0900

    Hotfix Passenger bug when using with WSL

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# For Passenger bug on Windows Subsystem for Linux (WSL)
+if /microsoft/i =~ File.open('/proc/version')&.read
+  ENV['_PASSENGER_FORCE_HTTP_SESSION'] = "true"
+end
