dff26f71692d2d005275905b4abcbd19926854eb
yuknak <yuknak4@gmail.com>
2019-01-17 06:57:48 +0900

    Add public/site1 resources

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -90,7 +90,7 @@ class TaskController < ApplicationController
       title = node.inner_text
       category_id = node.attribute('href').text
       category_id.sub!(/\//,"")
-      icon_url = "/orz/cate_" + category_id + ".png"
+      icon_url = "/site0/cate_" + category_id + ".png"
       if (/^\d+$/ =~ category_id)
         category = Category.find_or_initialize_by(
           ex_id: category_id
