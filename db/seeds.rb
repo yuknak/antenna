3f334034e46281a6777e21140fbfb40815a9877b
yuknak <yuknak4@gmail.com>
2019-01-20 16:08:48 +0900

    Remove seeds.rb

--- db/seeds.rb
+++
@@ -1,4 +0,0 @@
-print "No data in seeds.rb.\n"
-print "Please run \"bundle exec rails task:site\" to seed.\n"
-print "And after that, run \"bundle exec rails task:article\" to get from rss seeds.\n"
-print "And run \"bundle exec rails task:daily\".\n"
