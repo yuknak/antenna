d55fbdc19f190cc1970bcfde0ac9aa58f32061c0
yuknak <yuknak4@gmail.com>
2019-01-21 09:23:25 +0900

    Add ANTENNA_PASS_LOCK env value

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :basic
   
   private def basic
-    if ENV['RAILS_ENV'] != 'production' then
+    if ENV['RAILS_ENV'] != 'production' ||
+       ENV['ANTENNA_PASS_LOCK'] == '1' then
       authenticate_or_request_with_http_basic do |user, pass|
         user == CONFIG['admin']['user'] && pass == CONFIG['admin']['pass']
       end
